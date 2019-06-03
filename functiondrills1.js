6a2727d9e887de77f8a546069f3d15898be6662e
Blake Lowrey <blake.lowrey1@gmail.com>
2019-06-03 16:27:32 -0400

    the new new real new edit

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,13 +1,21 @@
 
 function createGreeting(name, age) {
-    return "Hi, my name is" + name + " and Im " + age " years old"
-    
+    if (typeof name === 'string' && typeof age === 'number'){
+        return "Hi, my name is" + name + " and I'm " + age  + " years old";
+    }
+    else {
+        throw new Error('arguments are not valid');
+
+    }
 }
 
-function getYearOfBirth(name, age) {
+function getYearOfBirth(age) {
+    if (age < 0){
+        throw new Error("Age can not be negative");    }
     const yearOfBirth = 2019 - age;
-    return "I was born in " + yearOfBirth;
+    return yearOfBirth
 }
+
 const greeting1 = createGreeting();
 console.log(greeting1);
 
