54c158cb726951c202b31bc8594fb7e4cce2a776
geneggy <geneggy@gmail.com>
2019-06-03 13:09:07 -0700

    2nd edit

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,5 +1,15 @@
 
-function createGreeting() {
-    console.log('Hi, my name is Eugene and I\'m 290 years old');
-}  
-createGreeting();
+function createGreeting(name, age) {
+    return "Hi, my name is" + name + " and Im " + age " years old"
+    
+}
+
+function getYearOfBirth(name, age) {
+    const yearOfBirth = 2019 - age;
+    return "I was born in " + yearOfBirth;
+}
+const greeting1 = createGreeting();
+console.log(greeting1);
+
+
+
